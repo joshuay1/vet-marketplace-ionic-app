56a88a235bd4ce0217369397452c52124991b904
Stanley <stanleys@student.unimelb.edu.au>
2017-08-17 17:41:22 +1000

    updated project firebase user auth

---
+++ FireBase/src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { LoginPage } from "../pages/login/login";
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule }from 'angularfire2/auth'; 
+import { RegisterPage }from "../pages/register/register";
+
+
+var config = {
+  apiKey: "AIzaSyD16Z_p-S5LgSceHoL3ob7gRNSqqFaPbWQ",
+  authDomain: "vetquoll-c22f9.firebaseapp.com",
+  databaseURL: "https://vetquoll-c22f9.firebaseio.com",
+  projectId: "vetquoll-c22f9",
+  storageBucket: "vetquoll-c22f9.appspot.com",
+  messagingSenderId: "1063752403506"
+};
+
+@NgModule({
+  declarations: [
+    MyApp,
+    LoginPage,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(config),
+    AngularFireAuthModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    LoginPage,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
