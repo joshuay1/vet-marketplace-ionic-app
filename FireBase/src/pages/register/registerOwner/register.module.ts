8132aaeb4ad914e408169a3b3eda837a76109a59
Callum Bradshaw <callumb@student.unimelb.edu.au>
2017-08-31 11:45:25 +1000

    bug fix

--- FireBase/src/pages/register/registerOwner/register.module.ts
+++ FireBase/src/pages/register/registerOwner/register.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-import { RegisterPage } from './register';
+import { RegisterOwnerPage } from './register';
 
 @NgModule({
   declarations: [
-    RegisterPage,
+    RegisterOwnerPage,
   ],
   imports: [
-    IonicPageModule.forChild(RegisterPage),
+    IonicPageModule.forChild(RegisterOwnerPage),
   ],
 })
 export class RegisterPageModule {}
--- FireBase/src/pages/register/registerVet/register.module.ts
+++ FireBase/src/pages/register/registerVet/register.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-import { RegisterPage } from './register';
+import { RegisterVetPage } from './register';
 
 @NgModule({
   declarations: [
-    RegisterPage,
+    RegisterVetPage,
   ],
   imports: [
-    IonicPageModule.forChild(RegisterPage),
+    IonicPageModule.forChild(RegisterVetPage),
   ],
 })
 export class RegisterPageModule {}
