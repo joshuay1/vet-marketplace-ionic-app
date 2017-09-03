d0aa8a0d88a668efa4df0d89fa901cfa6c10e11b
shoryec <shorye2301@gmail.com>
2017-09-03 20:01:09 +1000

    User verification added

--- backend/complete/src/main/java/server/petprofileservice/PetProfileController.java
+++ backend/complete/src/main/java/server/petprofileservice/PetProfileController.java
@@ -1,18 +1,17 @@
 package server.petprofileservice;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import server.petprofileservice.PetProfileData;
 import server.response.BasicResponse;
 
 import java.io.IOException;
 
 @RestController
 public class PetProfileController {
+    private int flag;
     @RequestMapping(value = "/postPetProfile",method = RequestMethod.POST)
     public BasicResponse postProfile(
             @RequestParam(value ="petId")String id,
@@ -30,7 +29,29 @@ public class PetProfileController {
         //TODO: String checking for input
         //String checking for each input
         PetProfileData data = new PetProfileData(petname,animalType, dob, animalBreed,uid);
-        ref.setValue(data);
-    return new BasicResponse("success",id,null);
+        DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("users/");
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                if(snapshot.hasChild(uid)){
+                    ref.setValue(data);
+                    flag = 0;
+                }
+                else {
+                    flag = 1;
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+
+            }
+        });
+
+        if (flag==1) {
+            return new BasicResponse("success", id, null);
+        }
+        else
+            return new BasicResponse("failure",id,"No such user exists");
     }
 }
