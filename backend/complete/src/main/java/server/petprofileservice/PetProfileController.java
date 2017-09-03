5007dff03df2973172eb2f8450e3d7c8a2b5f597
shoryec <shorye2301@gmail.com>
2017-09-03 23:18:59 +1000

    update pet functionality

--- backend/complete/src/main/java/server/petprofileservice/PetProfileController.java
+++ backend/complete/src/main/java/server/petprofileservice/PetProfileController.java
@@ -1,6 +1,13 @@
 package server.petprofileservice;
 
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
 import com.google.firebase.database.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.maps.GeocodingApi;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.GeocodingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,36 +15,36 @@ import org.springframework.web.bind.annotation.RestController;
 import server.response.BasicResponse;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 @RestController
 public class PetProfileController {
     private int flag;
-    @RequestMapping(value = "/postPetProfile",method = RequestMethod.POST)
-    public BasicResponse postProfile(
-            @RequestParam(value ="petId")String id,
-            @RequestParam(value ="userId") String uid,
-            @RequestParam(value="petname") String petname,
-            @RequestParam(value="dob")String dob,
-            @RequestParam(value="animalType") String animalType,
-            @RequestParam(value="breed") String animalBreed)
-    {
 
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("pets/"+id);
+    @RequestMapping(value = "/postPetProfile", method = RequestMethod.POST)
+    public BasicResponse postPetProfile(
+            @RequestParam(value = "petId") String id,
+            @RequestParam(value = "userId") String uid,
+            @RequestParam(value = "petname") String petname,
+            @RequestParam(value = "dob") String dob,
+            @RequestParam(value = "animalType") String animalType,
+            @RequestParam(value = "breed") String animalBreed) {
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("pets/" + id);
         //TODO: check token.id = id
 
 
         //TODO: String checking for input
         //String checking for each input
-        PetProfileData data = new PetProfileData(petname,animalType, dob, animalBreed,uid);
+        PetProfileData data = new PetProfileData(petname, animalType, dob, animalBreed, uid);
         DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("users/");
         userRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot snapshot) {
-                if(snapshot.hasChild(uid)){
+                if (snapshot.hasChild(uid)) {
                     ref.setValue(data);
                     flag = 0;
-                }
-                else {
+                } else {
                     flag = 1;
                 }
             }
@@ -48,10 +55,46 @@ public class PetProfileController {
             }
         });
 
-        if (flag==1) {
+        if (flag == 1) {
             return new BasicResponse("success", id, null);
+        } else
+            return new BasicResponse("failure", id, "No such user exists");
+    }
+
+    @RequestMapping(value = "/petProfileUpdate", method = RequestMethod.POST)
+    public BasicResponse petProfileUpdate(
+            @RequestParam(value = "petId") String id,
+            @RequestParam(value = "petname") String petname,
+            @RequestParam(value = "dob") String dob,
+            @RequestParam(value = "animalType") String animalType,
+            @RequestParam(value = "breed") String animalBreed) {
+        //check token id = user id
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("pets/"+id);
+        HashMap<String, Object> update = new HashMap<String,Object> ();
+        if(petname!= null && !petname.isEmpty()){
+            //logger.info("name of "+id+" changed to " + petname);
+            update.put("petName", petname);
         }
-        else
-            return new BasicResponse("failure",id,"No such user exists");
+
+        if(dob != null && !dob.isEmpty()){
+            //logger.info("dob of "+id+" changed to " + dob);
+            update.put("dob",dob);
+        }
+
+        if(animalBreed!= null && !animalBreed.isEmpty()){
+            //logger.info("animalBreed of "+id+" changed to " + animalBreed);
+            update.put("animalBreed",animalBreed);
+        }
+
+        if(animalType != null && !animalType.isEmpty()){
+            //logger.info("Animal Type of "+id+" changed to " + animalType);
+            update.put("animalType",animalType);
+        }
+
+        ref.updateChildren(update);
+        return new BasicResponse("success", id, null);
+
+
     }
 }
