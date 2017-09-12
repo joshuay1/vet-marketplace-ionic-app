a7dad55e6c7600c0fe818397091e06a6548b1436
Stanley <stanley_sim1@hotmail.com>
2017-09-12 15:34:22 +1000

    authService

--- backend/complete/src/main/java/server/HelperFunction.java
+++ backend/complete/src/main/java/server/HelperFunction.java
@@ -12,6 +12,8 @@ import java.util.Arrays;
 import java.util.concurrent.Semaphore;
 
 import javax.net.ssl.HttpsURLConnection;
+
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -101,6 +103,74 @@ public class HelperFunction {
 
 		
 
+
+	}
+
+
+	public static JSONArray getDataInArray(String path, Logger logger){
+		String token = null;
+		if(scope == null){
+			try{
+				File file = new File(Application.class.getResource("/service-account.json").toURI());
+				FileInputStream serviceAccount = new FileInputStream(file);
+				GoogleCredential googleCred = GoogleCredential.fromStream( serviceAccount);
+				scope = googleCred.createScoped(
+					Arrays.asList(
+				  	"https://www.googleapis.com/auth/firebase.database.readonly",  // or use firebase.database.readonly for read-only access
+				 	 "https://www.googleapis.com/auth/userinfo.email"
+					)
+				);
+			
+					
+			
+			}catch(IOException e){
+				logger.info("file not found service account");
+				return null;
+			}catch(URISyntaxException e){
+				logger.info("problem in reading the file");
+				return null;
+			}
+		}
+
+
+		try{
+			scope.refreshToken();
+		}catch(IOException e){
+			logger.info("problem in refreshing token");
+			return null;
+		}
+		token = scope.getAccessToken();	
+
+		String url = "https://vetquoll-c22f9.firebaseio.com/";
+		String restPath = url +path + ".json?access_token="+token;
+		
+		try{
+			URL obj = new URL(restPath);
+			HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+			con.setRequestMethod("GET");
+			logger.info("sending get rest request with path= "+ restPath);
+			int responseCode = con.getResponseCode();
+			logger.info("Response Code : " + responseCode);
+
+
+			BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+			JSONParser parser = new JSONParser();
+			return (JSONArray) parser.parse(response.toString());
+		}catch(IOException e){
+			logger.info("IO problems in connecting to firebase database");
+			return null;
+		}catch(ParseException e){
+			logger.info("parse problem from the database read");
+			return null;
+		}
 
 	}
 
