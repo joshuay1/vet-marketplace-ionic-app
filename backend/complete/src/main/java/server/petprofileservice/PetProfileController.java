c166934ff707e2d331a08d7fa3b014e76509b2cb
shoryec <shorye2301@gmail.com>
2017-09-12 13:08:33 +1000

    improved pushing petdata

--- backend/complete/src/main/java/server/petprofileservice/PetProfileController.java
+++ backend/complete/src/main/java/server/petprofileservice/PetProfileController.java
@@ -1,6 +1,8 @@
 package server.petprofileservice;
 
 import com.google.firebase.database.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -18,22 +20,85 @@ public class PetProfileController {
     private int flag = -1;
     private final Logger logger = LoggerFactory.getLogger(Application.class);
 
-
+    @CrossOrigin
     @RequestMapping(value = "/postPetProfile", method = RequestMethod.POST)
     public BasicResponse postPetProfile(
+            @RequestParam(value = "token") String tokenString,
+            @RequestBody String jsonString){
+        JSONParser parser = new JSONParser();
+        JSONObject jsonProfile = null;
+        try{
+            jsonProfile = (JSONObject) parser.parse(jsonString);
+        }catch(ParseException e) {
+            e.printStackTrace();
+            return new BasicResponse("error", null, "body not JSON Object");
+        }
+        /*
             @RequestParam(value = "petId") String id,
             @RequestParam(value = "userId") String uid,
             @RequestParam(value = "petname") String petname,
             @RequestParam(value = "dob") String dob,
             @RequestParam(value = "animalType") String animalType,
             @RequestParam(value = "breed") String animalBreed) {
+        */
+        //Initialize Data
+        String mid = null;
+        String muid = null;
+        String mpetname = null;
+        String mdob = null;
+        String manimalType = null;
+        String manimalBreed = null;
 
-        //TODO: check token.id = id
-        //DatabaseReference ref = FirebaseDatabase.getInstance().getReference("pets/");
+        if(jsonProfile.containsKey("userId")){
+            muid = (String) jsonProfile.get("userId");
+            //check token id = id
+            boolean match = HelperFunction.matchToken(muid, tokenString, logger);
 
-        //TODO: String checking for input
-        //String checking for each input
-        //PetProfileData data = new PetProfileData(petname, animalType, dob, animalBreed, uid);
+            if(match){
+                logger.info("id provided matches decoded token id. Continue");
+            }else{
+                logger.info("id provided does not match decoded token");
+                return new BasicResponse("error", muid, "id provided does not match decoded token");
+            }
+        }else{
+            return new BasicResponse("error", muid, "no user id found");
+        }
+        if(jsonProfile.containsKey("petId")){
+            mid = (String) jsonProfile.get("petId");
+        }
+        else{
+            return new BasicResponse("error",muid,"no petId found");
+        }
+        if(jsonProfile.containsKey("petname")){
+            mpetname = (String) jsonProfile.get("petname");
+        }
+        else {
+            return new BasicResponse("error",muid,"petname not provided");
+        }
+        if (jsonProfile.containsKey("dob")){
+            mdob = (String) jsonProfile.get("dob");
+        }
+        else{
+            return new BasicResponse("error",muid,"no dob provided");
+        }
+        if(jsonProfile.containsKey("animalType")){
+            manimalType = (String) jsonProfile.get("animalType");
+        }
+        else {
+            return new BasicResponse("error", muid,"no animalType provided");
+        }
+        if (jsonProfile.containsKey("breed")){
+            manimalBreed = (String) jsonProfile.get("breed");
+        }
+        else {
+            return new BasicResponse("error", muid, "no breed provided");
+        }
+        final String uid = muid;
+        final String id = mid;
+        final String petname = mpetname;
+        final String dob = mdob;
+        final String animalType = manimalType;
+        final String animalBreed = manimalBreed;
         DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("users/");
         userRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
