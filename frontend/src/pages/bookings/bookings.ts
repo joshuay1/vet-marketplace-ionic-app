14cc30913a1b3640c9e46b41acfea5e16c50500f
Stanley <stanley_sim1@hotmail.com>
2017-09-16 07:18:38 +1000

    start of booking

--- frontend/src/pages/bookings/bookings.ts
+++ frontend/src/pages/bookings/bookings.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FirebaseListObservable, AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { UserInfo } from '../../model/user';
 
 
 
@@ -9,14 +12,60 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'bookings.html',
 })
 export class BookingsPage {
-    bookings: string;
+    public currentBookings: FirebaseListObservable<any[]>;
+    public pastBookings : FirebaseListObservable<any[]>;
+    private userid: string;
 
-    constructor(public navCtrl: NavController, public navParams: NavParams) {
-        this.bookings = "currentBookings";
+    constructor(public navCtrl: NavController, 
+                public navParams: NavParams,
+                public db : AngularFireDatabase,
+                private af: AngularFireAuth) {
+
+
+        this.userid = this.af.auth.currentUser.uid;
+        this.getCurrentBooking();
+        this.getPastBooking();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad BookingsPage');
+  }
+
+  getCurrentBooking(){
+    console.log("get called");
+    this.currentBookings = this.db.list("bookings",{
+      query:{
+        orderByChild: 'userId',
+        equalTo: this.userid
+      }
+    });
+    console.log(this.currentBookings);
+  }
+
+  getPastBooking(){
+
+    //TODO
+
+  }
+
+  stringfy(json: any):string{
+
+    return JSON.stringify(json);
+  }
+
+  generateUserData(userid:any):string{
+    var profileData : FirebaseObjectObservable<UserInfo>;
+    profileData = this.db.object(`users/`+userid);
+    var response;
+    profileData.forEach(snapshot=>{
+      response =  snapshot.firstname+
+                     " " + snapshot.lastname
+                     /*"\n Address = " +snapshot.streetnumber +" " + snapshot.streetname+
+                     " , " + snapshot.suburb +" , "+ snapshot.state +" "+  snapshot.country*/;
+      
+    });
+    return response;
+    
   }
 
 }
