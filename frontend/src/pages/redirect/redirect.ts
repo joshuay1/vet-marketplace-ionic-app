27952b11da071630751bafd493bc35ed7897ec15
Stanley <stanleys@student.unimelb.edu.au>
2017-10-16 15:39:32 +1100

    redirect

--- frontend/src/pages/redirect/redirect.ts
+++ frontend/src/pages/redirect/redirect.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
-import {IonicPage, LoadingController, NavController, NavParams} from 'ionic-angular';
+import {IonicPage, LoadingController, NavController, NavParams, AlertController} from 'ionic-angular';
 import {OwnerHomePage} from "../home/ownerHome/ownerHome";
 import {VetHomePage} from '../home/vetHome/vetHome';
 import {User, UserInfo} from "../../model/user";
 import {AngularFireAuth} from "angularfire2/auth";
 import {AngularFireDatabase, FirebaseObjectObservable} from 'angularfire2/database';
+import { HttpServiceProvider } from '../../providers/http-service/http-service';
 
 /**
  * Generated class for the RedirectPage page.
@@ -19,15 +20,21 @@ import {AngularFireDatabase, FirebaseObjectObservable} from 'angularfire2/databa
   templateUrl: 'redirect.html',
 })
 export class RedirectPage {
-  userType: String;
-  vetKey: String;
-  profileData: FirebaseObjectObservable<UserInfo>;
-  loading: any;
+  private userType: String;
+  private vetKey: String;
+  private loading: any;
+  private isVerifiedVet: any;
+  private userid: string;
+  private apiUrl = 'http://115.146.86.193:8080/';
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams, private afAuth: AngularFireAuth,
               private db: AngularFireDatabase,
-              private loadingCtrl: LoadingController) {
+              private loadingCtrl: LoadingController,
+              private httpProviders : HttpServiceProvider,
+              private alertCtrl : AlertController) {
+    this.isVerifiedVet = this.navParams.get("isVerifiedVet");
+    console.log("is Verified Vet = "+ this.isVerifiedVet);
     this.loading = this.loadingCtrl.create({
       content: 'Logging In'
     });
@@ -51,30 +58,44 @@ export class RedirectPage {
     }
     if (this.userType == 'Vet') {
       //if is validated
-      var uid = this.afAuth.auth.currentUser.uid;
-      console.log("uid = " + uid);
-      this.profileData = this.db.object(`users/` + uid/*,{preserveSnapshot: true}*/);
-      this.profileData.forEach(snapshot => {
-        if (snapshot.isVerifiedVet) {
-          console.log("redirecting as vet");
-          this.loading.dismiss();
-          this.navCtrl.setRoot(VetHomePage);
+      this.userid = this.afAuth.auth.currentUser.uid;
+      console.log("uid = " + this.userid);
+      if (this.isVerifiedVet) {
+        console.log("redirecting as vet");
+        this.loading.dismiss();
+        this.navCtrl.setRoot(VetHomePage);
 
-        }
-        else {
-          var show = document.getElementById("authKey");
-          show.style.visibility = "visible"
+      }
+      else {
+        var show = document.getElementById("authKey");
+        show.style.visibility = "visible";
+        this.loading.dismiss();
           //make things seen!! ------------------- TODO
-        }
-      });
+      }
     }
 
   }
 
   verifyVet() {
-    if (true) {
-
-    }
+    console.log(this.vetKey);
+    var body = {userid : this.userid,authkey : this.vetKey };
+    this.httpProviders.httpPost(this.apiUrl+"verify", JSON.stringify(body))
+    .then(result=>{
+      console.log("get result here");
+      var res = result.response;
+      if(res =="success"){
+        console.log("get result here");
+        this.navCtrl.setRoot(VetHomePage);
+      }
+    }).catch(err=>{
+      console.log("catchin error here");
+      let alert = this.alertCtrl.create({
+        title: 'Error',
+        message : err,
+        buttons : ['OK']
+      });
+      alert.present();
+    });
   }
 
   back() {
